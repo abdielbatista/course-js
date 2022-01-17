14d15482fba11f6efe812bf69644346e68d70315
abdielbatista <abdielb@yahoo.com>
2022-01-17 20:07:00 -0300

    initial

---
+++ class.js
@@ -0,0 +1,31 @@
+//variavel
+let valorIngAdulto = 20;
+let name = 'abdiel'
+
+//objeto
+let pessoa = {
+    //chave valor
+    name: 'Abdiel',
+    idade: 21,
+    aprovado: true,
+    sobrenome: 'Batista'
+};
+
+//console.log(pessoa);
+
+//arrays
+let familia = ["21", "25", "32", "35"]
+
+/*console.log(familia)
+console.log(familia[2])*/
+
+//functions verbo + substantivo
+function somarNumeros(){
+    n1 = 2;
+    n2 = 3;
+
+    soma = n1 + n2;
+
+    console.log(soma);
+};
+
