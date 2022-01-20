461336e13295751c074a7c2cbf5114138a3eaf14
abdielbatista <abdielb@yahoo.com>
2022-01-19 21:39:22 -0300

    introduction

---
+++ function.js
@@ -0,0 +1,13 @@
+//realiza a tarefa e não retorna nada
+function dizerNome(){
+    console.log('abdiel')
+}
+
+dizerNome();
+
+//faz um calculo ou operação e retorna algo
+function mult(valor){
+    return valor * 2;
+}
+
+console.log(mult(5))
