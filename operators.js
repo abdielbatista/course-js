4c301e7521dacb4dd3e6076e6042acca2174a363
abdielbatista <abdielb@yahoo.com>
2022-01-31 21:37:45 -0300

    project-001

---
+++ operators.js
@@ -0,0 +1,11 @@
+let valor = 50
+
+console.log(valor + valor)
+console.log(valor - valor)
+console.log(valor * valor)
+console.log(valor / valor)
+console.log(5 ** 2)
+
+let pontos = 200
+let tipo = pontos > 100 ? 'premium' : 'comum'
+console.log(tipo)
