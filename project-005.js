599c539ce19bde555c177bdeec62f08771cfed58
abdielbatista <abdielb@yahoo.com>
2022-02-02 22:00:33 -0300

    project-005

---
+++ project-005.js
@@ -0,0 +1,14 @@
+const filme = {
+    nome: 'Vingadores',
+    ano: 2018,
+    personagem: 'Capitão America'
+}
+
+exibirString(filme);
+
+function exibirString(obj){
+    for(let indice in obj){
+        if(typeof obj[indice] == 'string')
+            console.log((indice,obj[indice]))
+    }
+}
