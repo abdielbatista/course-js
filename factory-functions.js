27cda9e6eb3aee5232b00a14430bd3ba32ca1a4f
abdielbatista <abdielb@yahoo.com>
2022-02-21 20:32:19 -0300

    factoryFunctions

---
+++ factory-functions.js
@@ -0,0 +1,25 @@
+function criarCelular(marcaCelular, tamanhoTela, capacidadeBateria){
+    return{
+        marcaCelular,
+        tamanhoTela,
+        capacidadeBateria,
+        ligar(){
+            console.log("ligando...")
+        }
+    }
+}
+
+const celular01 = criarCelular('ASUS',6.5,5000);
+console.log(celular01);
+
+function notebook(marca, cpu, hd, ram){
+    return{
+        marca,
+        cpu,
+        hd,
+        ram
+    }
+}
+
+const not01 = notebook('ACER', 'AMD', '1tb', '8gb')
+console.log(not01)
--- project-009.js
+++ project-009.js
@@ -4,14 +4,19 @@ exibirNumPrimos(15);
 
 function exibirNumPrimos(limite){
     for(let num = 2; num <= limite; num++){
-        let primo = true;
 
-        for(let div = 2; div < num; div++){
-            if(num % div === 0){
-                primo = false;
-                break;
-            }
-        }
-        if (primo) console.log(num)
+        if (ehPrimo(num) == true) console.log(num)
+    
     }
+}
+
+function ehPrimo(num){
+    let primo = true;
+    for(let div = 2; div < num; div++){
+        if(num % div === 0){
+            primo = false;
+        }
+    }
+
+    return primo
 }
